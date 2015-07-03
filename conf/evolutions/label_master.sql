c6e078deeaa32c19e81abee788768aa6e24a43ee
gowthamiv <gowthami.vasabhaktula@imaginea.com>
2015-07-03 14:52:07 +0530

    Script for adding default labels in label_master table.

---
+++ conf/evolutions/label_master.sql
@@ -0,0 +1,36 @@
+
+insert into label_master(label_id, name, description, owner_id) values (1, "Work", "Related to work", 1);
+insert into label_master(label_id, name, description, owner_id) values (2, "Personal", "Personal Information", 1);
+
+insert into label_master(label_id, name, description, owner_id) values (3, "Work", "Related to work", 2);
+insert into label_master(label_id, name, description, owner_id) values (4, "Personal", "Personal Information", 2);
+
+insert into label_master(label_id, name, description, owner_id) values (5, "Work", "Related to work", 3);
+insert into label_master(label_id, name, description, owner_id) values (6, "Personal", "Personal Information", 3);
+
+insert into label_master(label_id, name, description, owner_id) values (7, "Work", "Related to work", 4);
+insert into label_master(label_id, name, description, owner_id) values (8, "Personal", "Personal Information", 4);
+
+insert into label_master(label_id, name, description, owner_id) values (9, "Work", "Related to work", 5);
+insert into label_master(label_id, name, description, owner_id) values (10, "Personal", "Personal Information", 5);
+
+insert into label_master(label_id, name, description, owner_id) values (11, "Work", "Related to work", 6);
+insert into label_master(label_id, name, description, owner_id) values (12, "Personal", "Personal Information", 6);
+
+insert into label_master(label_id, name, description, owner_id) values (13, "Work", "Related to work", 7);
+insert into label_master(label_id, name, description, owner_id) values (14, "Personal", "Personal Information", 7);
+
+insert into label_master(label_id, name, description, owner_id) values (15, "Work", "Related to work", 8);
+insert into label_master(label_id, name, description, owner_id) values (16, "Personal", "Personal Information", 8);
+
+insert into label_master(label_id, name, description, owner_id) values (17, "Work", "Related to work", 9);
+insert into label_master(label_id, name, description, owner_id) values (18, "Personal", "Personal Information", 9);
+
+insert into label_master(label_id, name, description, owner_id) values (19, "Work", "Related to work", 10);
+insert into label_master(label_id, name, description, owner_id) values (20, "Personal", "Personal Information", 10);
+
+insert into label_master(label_id, name, description, owner_id) values (21, "Work", "Related to work", 11);
+insert into label_master(label_id, name, description, owner_id) values (22, "Personal", "Personal Information", 11);
+
+commit;
+
