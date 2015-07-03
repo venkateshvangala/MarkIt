a79db21c8a9b5baa4c049647c4198e02195ee90c
venkatesh <venkatesh.vanagala1989@gmail.com>
2015-07-03 12:24:07 +0530

    Login page UI changes

--- public/src/config/require-config.js
+++ public/src/config/require-config.js
@@ -2,14 +2,15 @@ requirejs.config({
 	
     "baseUrl": "assets/src/",
     "paths": {
-      "jquery" 		: "js/lib/jquery-1.9.0.min",
+      "jquery" 		: "js/lib/jquery-1.11.3.min",
       "jqueryUi"	: 'js/lib/jquery-ui',
       "underscore"	: 'js/lib/underscore-min',
       "backbone"	: 'js/lib/backbone-min',
       "chosen"		: 'js/lib/chosen.jquery',
       "noty"		: 'js/lib/jquery.noty',
       "handlebars"	: 'js/lib/handlebars',
-      "text"		: 'js/lib/text'
+      "text"		: 'js/lib/text',
+      "bootstrap"   : 'js/lib/bootstrap.min'
     },
     "shim" : {
         "underscore" : {
@@ -18,12 +19,16 @@ requirejs.config({
         "backbone" : {
             "deps"    : ["jquery", "underscore"],
             "exports" : "Backbone"
+        },
+        "chosen": {
+            deps: ["bootstrap"],
+      	    exports: "Chosen"  
         }
     }
 });
 
-require(["js/views/common/appView", "js/views/common/viewCreate", "js/controller/markit-router"],
-function(AppView, ViewCreate, MarkItRouter){
+require(["jquery", "js/views/common/appView", "js/views/common/viewCreate", "js/controller/markit-router", "chosen"],
+function(jQuery, AppView, ViewCreate, MarkItRouter, Chosen){
 	var appView = ViewCreate.create({}, 'AppView', AppView);
 	appView.render();
 	var markItRouter = new MarkItRouter({appView : appView}); 
