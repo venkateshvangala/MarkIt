a522a4d962b0738eb23324752237567e936a1862
klakshmi75 <lakshmi.k@imaginea.com>
2015-07-03 20:23:33 +0530

    - Added setter and getter methods in User entity bean

--- app/models/data/User.scala
+++ app/models/data/User.scala
@@ -8,51 +8,92 @@ import java.sql.Timestamp
  */
 
 @Entity
-@Table(name="mi_user")
+@Table(name = "mi_user")
 class User {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "user_id")
+  val _userId: Long = 0;
+
+  @Column(name = "display_name")
+  var _displayName: String = null;
+
+  @Column(name = "email")
+  var _email: String = null;
+
+  @Column(name = "password")
+  var _password: String = null;
+
+  @Column(name = "mobile")
+  var _mobile: String = null;
+
+  @Column(name = "first_name")
+  var _firstName: String = null;
+
+  @Column(name = "last_name")
+  var _lastName: String = null;
+
+  @Column(name = "gender")
+  var _gender: String = null;
+
+  @Column(name = "age")
+  var _age: Int = 0;
+
+  @Column(name = "role")
+  var _role: String = null;
+
+  @Column(name = "last_updated")
+  var _lastUpdated: Timestamp = null;
+
+  @Column(name = "updated_by")
+  var _updatedBy: String = null;
+
+  @Column(name = "created_by")
+  var _createdBy: String = null;
+
+  @Column(name = "date_created")
+  var _dateCreated: Timestamp = null;
+
   
-    @Id
-    @GeneratedValue
-    @Column(name = "user_id")
-    val userId: Long = 0;
-    
-    @Column(name = "display_name")
-    val displayName: String = null;
-    
-    @Column(name = "email")
-    val email: String = null;
-    
-    @Column(name = "password")
-    val password: String = null;
-    
-    @Column(name = "mobile")
-    val mobile: String = null;
-    
-    @Column(name = "first_name")
-    val firstName: String = null;
-    
-    @Column(name = "last_name")
-    val lastName: String = null;
-    
-    @Column(name = "gender")
-    val gender: String = null;
-    
-    @Column(name = "age")
-    val age: Int = 0;
-    
-    @Column(name = "role")
-    val role: String = null;
-    
-    @Column(name = "last_updated")
-    val lastUpdated: Timestamp = null;
-    
-    @Column(name = "updated_by")
-    val updatedBy: String = null;
-    
-    @Column(name = "created_by")
-    val createdBy: String = null;
-    
-    @Column(name = "date_created")
-    val dateCreated: Timestamp = null;
-    
+
+  def displayName = _displayName;
+  def displayName_=(value: String): Unit = _displayName = value
+
+  def email = _email;
+  def email_=(value: String): Unit = _email = value
+
+  def password = _password;
+  def password_=(value: String): Unit = _password = value
+
+  def mobile = _mobile;
+  def mobile_=(value: String): Unit = _mobile = value
+
+  def firstName = _firstName;
+  def firstName_=(value: String): Unit = _firstName = value
+
+  def lastName = _lastName;
+  def lastName_=(value: String): Unit = _lastName = value
+
+  def gender = _gender;
+  def gender_=(value: String): Unit = _gender = value
+
+  def  age = _age;
+  def age_=(value: Int): Unit = _age = value
+
+  def role = _role;
+  def role_=(value: String): Unit = _role = value
+
+  def lastUpdated = _lastUpdated;
+  def lastUpdated_=(value: Timestamp): Unit = _lastUpdated = value
+
+  def updatedBy = _updatedBy;
+  def updatedBy_=(value: String): Unit = _updatedBy = value
+
+  def createdBy = _createdBy;
+  def createdBy_=(value: String): Unit = _createdBy = value
+
+  def dateCreated = _dateCreated;
+  def dateCreated_=(value: Timestamp): Unit = _dateCreated = value
+
 }
